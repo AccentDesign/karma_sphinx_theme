a23c0289b7e2739fb9b7e4a4d5a900e3290431f8
Stuart George <stuart.bigmassa@gmail.com>
2018-02-03 00:42:22 +0000

    update to run css from package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,9 @@
 var $ = require('gulp-load-plugins')();
 var gulp = require('gulp');
-var runSequence = require('run-sequence');
-
-// postcss
-var autoprefixer = require('autoprefixer');
-var discardempty = require('postcss-discard-empty');
-var discardcomments = require('postcss-discard-comments');
 
 var config = {
     publicDir: 'karma_sphinx_theme/static',
     scssDir: 'scss',
-    autoprefixer: {
-        browsers: [
-            'Chrome >= 35',
-            'Firefox >= 38',
-            'Edge >= 12',
-            'Explorer >= 10',
-            'iOS >= 8',
-            'Safari >= 8',
-            'Android 2.3',
-            'Android >= 4',
-            'Opera >= 12'
-        ],
-        "cascade": false
-    },
-    sass: {
-        "includePaths": [
-            'node_modules'
-        ]
-    },
-    cssmin: {},
     fontello: {
         config: 'scss/config.json',
         options: {
@@ -40,19 +14,6 @@ var config = {
         }
     }
 };
-
-gulp.task('scss', function() {
-    return gulp.src(config.scssDir + '/theme.scss')
-    .pipe($.sourcemaps.init())
-    .pipe($.sass(config.sass))
-    .pipe($.postcss([
-        discardcomments(),
-        discardempty(),
-        autoprefixer(config.autoprefixer)
-    ]))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest(config.publicDir + '/css'));
-});
 
 gulp.task('fontello', function () {
     return gulp.src(config.fontello.config)
@@ -66,29 +27,7 @@ gulp.task('fontello', function () {
     .pipe(gulp.dest(config.scssDir))
 });
 
-gulp.task('fontello_dist_fonts', function () {
+gulp.task('fontello_dist', function () {
     return gulp.src(config.scssDir + '/fonts/*')
     .pipe(gulp.dest(config.publicDir + '/fonts'))
-});
-
-gulp.task('min', function () {
-  return gulp.src(config.publicDir + '/css/theme.css')
-    .pipe($.cssmin(config.cssmin))
-    .pipe($.rename({suffix: '.min'}))
-    .pipe(gulp.dest(config.publicDir + '/css'))
-    .pipe($.livereload());
-});
-
-gulp.task('default', function(cb) {
-    runSequence('scss', 'min', 'fontello_dist_fonts', cb);
-});
-
-gulp.task('watch', ['default'], function (){
-    $.livereload.listen();
-    gulp.watch(
-        [
-            config.scssDir + '/*.scss',
-            config.scssDir + '/**/*.scss'
-        ],
-        ['default']);
 });
